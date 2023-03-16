7a5531aa2ce200ed76fdfd1b93d4b50537b5c965
Felipe Santana <felipe.jesus0801@gmail.com>
2023-03-16 12:29:43 -0300

    upt: db

--- prisma/migrations/20230316152712_create_db_on_railwail_with_postgress/migration.sql
+++ prisma/migrations/20230316152712_create_db_on_railwail_with_postgress/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "linkedin" TEXT NOT NULL,
     "github" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
